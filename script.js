b3d6a07763cf85e67398b5122d9de265b2382baf
Rafael Willen <rafaelpadre20@gmail.com>
2021-08-30 16:37:23 +0100

    Add the transactions objects with the transactions and methods for expenses, incomes and total

--- script.js
+++ script.js
@@ -4,6 +4,35 @@ const Modal = {
   },
 };
 
+const Transaction = {
+  all: [
+    {
+      description: "App",
+      value: 1000000,
+      date: "26/08/2021",
+    },
+    {
+      description: "Laptop",
+      value: -100000,
+      date: "27/08/2021",
+    },
+  ],
+  incomes() {
+    return Transaction.all.reduce((prev, curr) => {
+      if (curr.value > 0) prev += curr.value;
+      return prev;
+    }, 0);
+  },
+  expenses() {
+    return Transaction.all.reduce((prev, curr) => {
+      if (curr.value < 0) prev += curr.value;
+      return prev;
+    }, 0);
+  },
+  total() {
+    return this.incomes() + this.expenses();
+  },
+};
 // An IIFE. Runs the function immediately
 (() => {
   document.querySelector("button.add").onclick = Modal.toggle;
